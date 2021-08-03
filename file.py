82ead051e42d0164814435c9cbd68054061f50ad
Jeremy Okamoto <okamotojeremy@gmail.com>
2021-08-03 11:50:19 -0700

    a

--- file.py
+++ file.py
@@ -19,6 +19,26 @@ class BankAccount:
         self.balance -= amount
         other_user.balance += amount
         return self
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.balance = 0
+        BankAccount 
+    def make_deposit(self, amount):
+        self.balance += amount
+        return self
+    def make_withdrawl(self, amount):
+        self.balance -= amount
+        return self
+    def display_user_balance(self):
+        print(self.name + "'s balance: " +str(self.balance))
+        return self
+    def transfer_money(self, other_user, amount):
+        self.balance -= amount
+        other_user.balance += amount
+        return self
+
 jeremy = BankAccount("Jeremy")
 jeremy.deposit(500).deposit(300).deposit(200).withdraw(500).yield_interest().display_account_info()
 bob = BankAccount("bob")
